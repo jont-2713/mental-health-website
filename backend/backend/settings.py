61055cce1ade666117d26914e66b077b00964844
jont-2713 <jontygodfrey@outlook.com>
2025-07-29 21:29:03 +1000

    Restructed html templates folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # global templates here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
